b7920fe61efb543be057539f6b1372be17cb6474
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2020-09-15 15:29:49 -0500

    Completed project

---
+++ ControlsAndCompoundConditionBrady/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ControlsAndCompoundConditionBrady
+{
+    partial class TimeZone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.cityListBox = new System.Windows.Forms.ListBox();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.timeZoneLabel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.submitCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Location = new System.Drawing.Point(30, 19);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(291, 17);
+            this.promptLabel.TabIndex = 0;
+            this.promptLabel.Text = "Select a city and I will give you the time zone.";
+            // 
+            // cityListBox
+            // 
+            this.cityListBox.FormattingEnabled = true;
+            this.cityListBox.ItemHeight = 16;
+            this.cityListBox.Items.AddRange(new object[] {
+            "Denver",
+            "Honolulu",
+            "Minneapolis",
+            "New York",
+            "San Francisco"});
+            this.cityListBox.Location = new System.Drawing.Point(84, 52);
+            this.cityListBox.Name = "cityListBox";
+            this.cityListBox.Size = new System.Drawing.Size(175, 84);
+            this.cityListBox.TabIndex = 1;
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(81, 152);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(80, 17);
+            this.outputDescriptionLabel.TabIndex = 2;
+            this.outputDescriptionLabel.Text = "Time Zone:";
+            // 
+            // timeZoneLabel
+            // 
+            this.timeZoneLabel.AutoSize = true;
+            this.timeZoneLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeZoneLabel.Location = new System.Drawing.Point(206, 153);
+            this.timeZoneLabel.Name = "timeZoneLabel";
+            this.timeZoneLabel.Size = new System.Drawing.Size(2, 19);
+            this.timeZoneLabel.TabIndex = 3;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(69, 242);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "&OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(184, 244);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // submitCheckBox
+            // 
+            this.submitCheckBox.AutoSize = true;
+            this.submitCheckBox.Location = new System.Drawing.Point(135, 197);
+            this.submitCheckBox.Name = "submitCheckBox";
+            this.submitCheckBox.Size = new System.Drawing.Size(73, 21);
+            this.submitCheckBox.TabIndex = 6;
+            this.submitCheckBox.Text = "Submit";
+            this.submitCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // TimeZone
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(360, 311);
+            this.Controls.Add(this.submitCheckBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.timeZoneLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.cityListBox);
+            this.Controls.Add(this.promptLabel);
+            this.Name = "TimeZone";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.ListBox cityListBox;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Label timeZoneLabel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.CheckBox submitCheckBox;
+    }
+}
+
---
+++ ControlsAndCompoundConditionBrady/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************
+* Name        : Time Zone
+* Author      : Kabrina Brady
+* Created     : 9/15/2019
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work
+* Description : User selects a city from the list, checks CheckBox, clicks OK, and time zone of that city is displayed
+*               Input:  Mouse clicks or keyboard
+*               Output: Time zone
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or unmodified. I have not given other fellow student(s) access to my program.         
+***************************************************************/
+
+namespace ControlsAndCompoundConditionBrady
+{
+    public partial class TimeZone : Form
+    {
+        public TimeZone()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            if (submitCheckBox.Checked == true) //runs program if CheckBox is checked
+            {
+                string city; //holds name of a city
+                if (cityListBox.SelectedIndex != -1)
+                {
+                    //get selected item
+                    city = cityListBox.SelectedItem.ToString();
+
+                    //determine time zone
+                    if (cityListBox.SelectedItem == "Honolulu")
+                    {
+                        timeZoneLabel.Text = "Hawaii-Aleutian";
+                    }
+                    else if (cityListBox.SelectedItem == "San Francisco")
+                    {
+                        timeZoneLabel.Text = "Pacific";
+                    }
+                    else if (cityListBox.SelectedItem == "Denver")
+                    {
+                        timeZoneLabel.Text = "Mountain";
+                    }
+                    else if (cityListBox.SelectedItem == "New York")
+                    {
+                        timeZoneLabel.Text = "Eastern";
+                    }
+                    else if (cityListBox.SelectedItem == "Minneapolis")
+                    {
+                        timeZoneLabel.Text = "Central";
+                    }
+                }
+                else
+                {
+                    //no city was selected
+                    MessageBox.Show("Select a city.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please check the box."); //prompts the user to check the box if hasn't been checked
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            //closes the form
+            this.Close();
+        }
+    }
+}
